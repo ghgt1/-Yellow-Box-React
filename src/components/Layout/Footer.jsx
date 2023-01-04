9c3edfa885774fe20f705429c1b7538e49091d3c
JYROH <ghgt1@naver.com>
2023-01-04 13:24:16 +0900

    feat: Create Footer Layout

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>YELLOW&nbsp;</span>
+      <span>BOX</span>
+      <h3>© 2022 JYROH, All rights reserved.</h3>
+    </footer>
+  );
+};
+
+export default Footer;
