45fe73997f7d2af1c7ad83f043ca4d2fda1b6a08
JYROH <ghgt1@naver.com>
2023-01-04 17:02:40 +0900

    feat: Add GetMovies Componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
+import Main from "./pages/Main";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route index element={<Main />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
