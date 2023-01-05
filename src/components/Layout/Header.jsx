527e124c1137ae7ce11f44222a98d1f6d4aca1d3
JYROH <ghgt1@naver.com>
2023-01-05 16:51:13 +0900

    feat: Add cleanup Function on Link Tag to reload

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import boxIconEl from "../../assets/favicon2.png";
 import githubEl from "../../assets/github.png";
 
 const Header = () => {
+  // 와 유즈이펙트 이렇게 쓰는건가... 언마운트될때 상정하고 클린업만 사용
+  const useHomeHandler = () => {
+    useEffect(() => {
+      return () => {
+        window.location.reload();
+      };
+    }, []);
+  };
+
   return (
     <header>
       <div className={styles["left-container"]}>
-        <Link to="/">
+        <Link to="/" onClick={useHomeHandler}>
           <img src={boxIconEl} alt="노랑박스로고" />
           <span>YELLOW&nbsp;</span>
           <span> BOX</span>
