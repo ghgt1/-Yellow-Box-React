97ff6969696233dd2109a0340cc7bcef28928d1a
JYROH <ghgt1@naver.com>
2023-01-05 16:52:38 +0900

    test: Test Debounce

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -3,16 +3,17 @@ import { useEffect, useState } from "react";
 export const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
+  // useEffect를 사용한후는 settimeout자체가 사이드이펙트... 언마운트될때 제거해주기위해서인듯?
+
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
-    //value값이 바뀌었을때 handler 를 없애준다.
     return () => {
       clearTimeout(handler);
     };
   }, [value, delay]);
-
+  console.log(debouncedValue);
   return debouncedValue;
 };
