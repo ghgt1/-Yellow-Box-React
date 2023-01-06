e73e57f4f3137658a80a3298df88b782fff91269
JYROH <ghgt1@naver.com>
2023-01-07 01:35:05 +0900

    feat: Make page go top on route

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollRestore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
